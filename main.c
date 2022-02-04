1845ba9cd7df9cb508219c8b01caee33e58cbb16
yakishamo <82935423+yakishamo@users.noreply.github.com>
2022-02-04 19:18:47 +0900

    ファイルを配列に読み込むだけ

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//reallocして失敗したら終了する関数.
+void *my_realloc(char *ptr, int size){
+	ptr = realloc(ptr, size);
+	if(ptr == NULL){
+		fprintf(stderr, "realloc error.\n");
+		exit(1);
+	}
+	return ptr;
+}
+
+int main(int argc, char **argv) {
+	
+	if(argc == 1){
+		fprintf(stderr, "%s: No file specified.\n", argv[0]);
+		exit(1);
+	}
+
+	FILE *fp;
+	if((fp = fopen(argv[1], "r")) == NULL){
+		fprintf(stderr, "%s: Cannot open %s\n", argv[0], argv[1]);
+		exit(1);
+	}
+
+	char *cmd = (char*)malloc(sizeof(char)*1000);
+	int cmd_len = 1000;
+	int i = 0;
+	char chr;
+	
+	//一文字ずつファイルの内容を配列に読み込む.
+	while((chr = fgetc(fp)) != EOF) {
+		cmd[i++] = chr;
+		if(i % 1000 == 0) {		//領域が足りなくなったら拡張.
+			cmd = (char*)my_realloc(cmd, sizeof(char)*1000 + cmd_len);
+			cmd_len += 1000;
+		}
+	}
+
+	if(i%1000 == 0) {
+		cmd = (char*)my_realloc(cmd, sizeof(char) + cmd_len);
+		cmd_len++;
+	}
+	cmd[++i] = '\0';
+
+	//テスト用, 読み込んだコマンド列をそのまま出力.
+	printf("%s", cmd);
+	printf("%d", cmd[i-1]);
+
+	free(cmd);
+	return 0;
+}
